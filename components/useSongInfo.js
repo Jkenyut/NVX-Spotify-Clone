bac3b9b2f319db820a3d09161d079d253ad56141
SATRIA NUR SAPUTRO <satrianursaputro06@gmail.com>
2022-02-13 23:46:24 +0700

    finally spotify yt v1

---
+++ components/useSongInfo.js
@@ -0,0 +1,32 @@
+import Player from './Player'
+
+import { useRecoilState } from 'recoil'
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom'
+import useSpotify from '../hooks/useSpotify'
+import { useEffect, useState } from 'react'
+
+function useSongInfo() {
+    const spotifyApi = useSpotify()
+    const [currentTrackId,setCurrentTrackId] = useRecoilState(currentTrackIdState)
+    const [isplaying,setIsPlaying] = useRecoilState(isPlayingState)
+    const [songInfo,setSongInfo] = useState(null)
+
+    useEffect(()=>{
+        const fetchSongInfo = async ()=>{
+            if(currentTrackId){
+                const trackinfo = await fetch(
+                    `https://api.spotify.com/v1/tracks/${currentTrackId}`,{
+                        headers:{
+                            Authorization : `Bearer ${spotifyApi.getAccessToken()}`
+                        }
+                    }
+                ).then(res => res.json())
+                setSongInfo(trackinfo)
+            }
+        }
+        fetchSongInfo()
+    },[currentTrackId,spotifyApi])
+  return songInfo
+}
+
+export default useSongInfo
---
+++ pages/index.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
+import Center from '../components/Center'
+import Player from '../components/Player'
+
+import { getSession } from 'next-auth/react'
+
+export default function Home() {
+  return (
+    <div className="h-screen overflow-hidden bg-black">
+      <Head>
+        <title>spotify 2.0</title>
+      </Head>
+
+      <main className="flex">
+        {/* sidebar */}
+        <Sidebar />
+        {/* Center */}
+        <Center />
+      </main>
+
+      {/* player */}
+      <div className='sticky bottom-0'>
+        <Player/>
+      </div>
+      
+    </div>
+  )
+}
+
+export async function getServerSideProps(context){
+  const session = await getSession(context)
+  return{
+    props:{
+      session,
+    }
+  }
+}
